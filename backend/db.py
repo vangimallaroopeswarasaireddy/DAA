2b30681195e7e0a2eb8e4e7db9e3c5cb672fa4dc
Vangimalla Roopeswara Sai Reddy <roopeswara123456@gmail.com>
2026-03-14 15:50:36 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -13,6 +13,11 @@ DB_NAME = os.getenv("DB_NAME") or os.getenv("MYSQLDATABASE") or "waterapp"
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+print("DB_HOST =", DB_HOST)
+print("DB_PORT =", DB_PORT)
+print("DB_NAME =", DB_NAME)
+print("DB_USER =", DB_USER)
+
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
